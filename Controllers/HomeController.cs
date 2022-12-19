55ed1de188076eca42bc0485cc9a2bf261b6fc91
Walid Sboui <walid.sboui@insat.ucar.tn>
2022-12-19 14:41:33 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Debug.WriteLine("Opening connection ");
+            SQLiteConnection dbCon = new SQLiteConnection(@"Data Source=C:\\Users\\USER\\Desktop\\.NET\\TP3.db;");
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info", dbCon);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        int id = (int)reader["id"];
+                        string first_name = (string)reader["first_name"];
+                        string last_name = (string)reader["last_name"];
+                        string email = (string)reader["email"];
+                        //   DateTime date_birth = Convert.ToDateTime((string)reader["date_birth"].ToString());
+                        string image = (string)reader["image"];
+                        string country = (string)reader["country"];
+
+                        Debug.WriteLine("id = {0} first name = {1}  last name = {2}  email={3}  image= {4}  country= {5}  date=", id, first_name, last_name, email, image, country);
+                    }
+
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP3.Models;
+
+namespace TP3.Controllers
+{
+    public class PersonController : Controller
+    {
+
+            [Route("Person/{id:int}")]
+            public ActionResult index(int id)
+            {
+                Personal_info personal_Info = new Personal_info();
+
+                return View(personal_Info.GetPerson(id));
+            }
+            [Route("Person/")]
+            public ActionResult all()
+            {
+                Personal_info personal_Info = new Personal_info();
+
+                return View(personal_Info.GetAllPerson());
+            }
+
+            //GET request
+            public ActionResult Search()
+            {
+                ViewBag.notFound = false;
+                return View();
+            }
+
+
+
+
+            [HttpPost]
+            public ActionResult Search(String first_name, String country)
+            {
+                Personal_info personal_info = new Personal_info();
+                List<Person> personal__info = personal_info.GetAllPerson();
+                foreach (Person person in personal__info)
+                {
+                    if (person.first_name == first_name && person.country == country)
+                    {
+                        return Redirect(person.id.ToString());
+
+                    }
+                }
+                ViewBag.notFound = true;
+
+
+
+                return View();
+             }
+    }
+}
